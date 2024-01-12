420a1d665a59e973651e5a59af12eabd9fc008fd
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2024-01-12 18:53:21 +0530

    Added sample.

---
+++ SerializeAndDeserialize/SerializeAndDeserialize/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SerializeAndDeserialize;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SerializeAndDeserialize/SerializeAndDeserialize/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SerializeAndDeserialize;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
