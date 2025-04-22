684e8812be48f61aa31111f3a89b9d451e47c28a
NatarajanSF4844 <natarajan.rangasamy@syncfusion.com>
2025-04-22 11:03:29 +0530

    Version Update
    950873-Updated .NET Version for Scheduler sample
    Description:
    Updated .NET 9 Version
    Fixed x:datatype warnings.
    Fixed null warnings
    Fixed Obsolote warnings
    Related Ticket Link: https://support.syncfusion.com/agent/tickets/713036

--- SerializeAndDeserialize/SerializeAndDeserialize/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SerializeAndDeserialize;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- SerializeAndDeserialize/SerializeAndDeserialize/Converter/JsonColorConverter.cs
+++ SerializeAndDeserialize/SerializeAndDeserialize/Converter/JsonColorConverter.cs
@@ -4,14 +4,23 @@ namespace SerializeAndDeserialize
 {
     public class JsonColorConverter : JsonConverter<Brush>
     {
-        public override Brush ReadJson(JsonReader reader, Type objectType, Brush existingValue, bool hasExistingValue, JsonSerializer serializer)
+        public override Brush ReadJson(JsonReader reader, Type objectType, Brush? existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
+            if(reader.Value == null)
+            {
+                return Colors.Black;
+            }
             string s = (string)reader.Value;
             return new SolidColorBrush(Color.FromArgb(s));
         }
 
-        public override void WriteJson(JsonWriter writer, Brush value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, Brush? value, JsonSerializer serializer)
         {
+            if(value == null)
+            {
+                return;
+            }
+
             writer.WriteValue(((SolidColorBrush)value).Color.ToHex());
         }
     }
